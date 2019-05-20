c129910b5c2dd9f6423f49044bbfec4f5015a757
unknown <lopatavlad@gmail.com>
2019-05-21 02:40:55 +0300

    second algorithm

--- LocalFirst.py
+++ LocalFirst.py
@@ -3,6 +3,8 @@ Local search algorithm
 '''
 def local_first(_list=[], n = 10, m = 3, verbose=False):
 	
+	import Util as u
+
 	#Calculating initial SPT
 	import SPT
 	matrix = SPT.SPT(_list, n, m, verbose)
@@ -21,8 +23,8 @@ def local_first(_list=[], n = 10, m = 3, verbose=False):
 		end_iterator = m-1
 
 		while start_iterator < end_iterator:
-			if _swap_calculate(matrix, start_iterator, i, end_iterator, i):
-				_matrix_swap(matrix, start_iterator, i, end_iterator, i)
+			if u.swap_calculate(matrix, start_iterator, i, end_iterator, i):
+				u.matrix_swap(matrix, start_iterator, i, end_iterator, i)
 			start_iterator += 1
 			end_iterator -= 1
 
@@ -37,33 +39,3 @@ def local_first(_list=[], n = 10, m = 3, verbose=False):
 			print("{}: {}".format(i+1, matrix[i]))
 
 	return matrix
-		
-	
-	 
-'''
-Helper function to decide whether swap is needed
-
-Tries to swap matrix elements and recalculates target function.
-'''
-def _swap_calculate(_matrix, row1, column1, row2, column2):
-	matrix = []
-	for i in range(len(_matrix)):
-		matrix.append(_matrix[i].copy())
-
-	C1 = sum(matrix[row1])
-	C2 = sum(matrix[row2])
-
-	_matrix_swap(matrix, row1, column1, row2, column2)
-
-	C1_new = sum(matrix[row1])
-	C2_new = sum(matrix[row2])
-
-	if abs(C1-C2) > abs(C1_new-C2_new):
-		return True
-	return False
-
-'''
-Helper function to swap matrix elements
-'''
-def _matrix_swap(_matrix, row1, column1, row2, column2):
-	_matrix[row1][column1], _matrix[row2][column2] = _matrix[row2][column2], _matrix[row1][column1]
---
+++ Util.py
@@ -0,0 +1,60 @@
+'''
+Helper function to decide whether swap is needed
+
+Tries to swap matrix elements and recalculates target function.
+'''
+def swap_calculate(_matrix, row1, column1, row2, column2):
+	matrix = []
+	for i in range(len(_matrix)):
+		matrix.append(_matrix[i].copy())
+
+	C1 = sum(matrix[row1])
+	C2 = sum(matrix[row2])
+
+	matrix_swap(matrix, row1, column1, row2, column2)
+
+	C1_new = sum(matrix[row1])
+	C2_new = sum(matrix[row2])
+
+	return abs(C1-C2) - abs(C1_new-C2_new)
+
+def radial_swap_calculate(_matrix, row1, column1, row2, column2):
+	matrix = []
+	for i in range(len(_matrix)):
+		matrix.append(_matrix[i].copy())
+
+	CM1 = target_C_macron(matrix)
+	Cmax1 = target_C_max(matrix)
+
+	matrix_swap(matrix, row1, column1, row2, column2)
+
+	CM2 = target_C_macron(matrix)
+	Cmax2 = target_C_max(matrix)
+
+	return CM2-CM1, Cmax2-Cmax1
+
+'''
+Helper function to swap matrix elements
+'''
+def matrix_swap(_matrix, row1, column1, row2, column2):
+	_matrix[row1][column1], _matrix[row2][column2] = _matrix[row2][column2], _matrix[row1][column1]
+
+def target_C_macron(matrix):
+	
+	n = 0
+	for i in range(len(matrix)):
+		n+=len(matrix[i])
+
+	sum = 0
+
+	for j in range(len(matrix)):
+		for k in range(len(matrix[j])):
+			sum += (len(matrix[j])-k)*matrix[j][k]
+
+	return (sum/n)
+
+def target_C_max(matrix):
+	sums = []
+	for j in range(len(matrix)):
+		sums.append(sum(matrix[j]))
+	return max(sums)
